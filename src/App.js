7f641ce4b812e081b4f7348fa3b485e8601b112d
yalejop <alejo.9211@hotmail.com>
2022-09-29 13:33:26 -0500

    initialize react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="container mt-4">
+        <h1>Hello Dojo!</h1>
+        <h3>Things I need to do:</h3>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a Marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </div>
+    );
+  }
+
+
+
 }
 
 export default App;
