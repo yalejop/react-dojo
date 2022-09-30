518e42d07b9de5bcfb1d1486dcef048483012c7b
yalejop <alejo.9211@hotmail.com>
2022-09-30 16:53:19 -0500

    create state variable for age y create a function for sum one to age

--- src/components/PersonalCard.js
+++ src/components/PersonalCard.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 class PersonalCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            edad: this.props.age
+        }
+    }
+
+    birthday = () => {
+        this.setState({edad : this.state.edad+1})
+    }
 
     render() {
         const {firstName, lastName, age, hairColor} = this.props
@@ -8,8 +18,9 @@ class PersonalCard extends React.Component {
         return (
             <div className="container card mb-3">
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.edad}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button className="btn btn-success" onClick={this.birthday}>Birthday Button for {firstName} {lastName}</button>
             </div>
         );
     }
