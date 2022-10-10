da45e51d44afd6479fbe59d5b0623602a3736071
Alejandro Cataño Herrera <alejo.9211@hotmail.com>
2022-10-10 12:41:51 -0500

    change class component to functional component

--- src/components/PersonalCard.js
+++ src/components/PersonalCard.js
@@ -1,32 +1,44 @@
 import React from "react";
 
-class PersonalCard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            edad: this.props.age
-        }
-    }
+// class PersonalCard extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             edad: this.props.age
+//         }
+//     }
 
-    birthday = () => {
-        this.setState({edad : this.state.edad+1})
-    }
+//     birthday = () => {
+//         this.setState({edad : this.state.edad+1})
+//     }
 
-    render() {
-        const {firstName, lastName, age, hairColor} = this.props
+//     render() {
+//         const {firstName, lastName, age, hairColor} = this.props
 
-        return (
-            <div className="container card mb-3">
-                <h1>{lastName}, {firstName}</h1>
-                <p>Age: {this.state.edad}</p>
-                <p>Hair Color: {hairColor}</p>
-                <button className="btn btn-success" onClick={this.birthday}>Birthday Button for {firstName} {lastName}</button>
-            </div>
-        );
-    }
+//         return (
+//             <div className="container card mb-3">
+//                 <h1>{lastName}, {firstName}</h1>
+//                 <p>Age: {this.state.edad}</p>
+//                 <p>Hair Color: {hairColor}</p>
+//                 <button className="btn btn-success" onClick={this.birthday}>Birthday Button for {firstName} {lastName}</button>
+//             </div>
+//         );
+//     }
     
 
-}
+// }
+
+const PersonalCard = ({firstName, lastName, age, hairColor}) => {
+    
+    return (
+        <div className="container card mb-3">
+            <h1>{lastName}, {firstName}</h1>
+            <p>Age: {age}</p>
+            <p>Hair Color: {hairColor}</p>
+            {/* <button className="btn btn-success">Birthday Button for {firstName} {lastName}</button> */}
+        </div>
+    );
+};
 
 
 export default PersonalCard
